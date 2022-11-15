52ac7304cda193e9ef5be30c28021a38e76f33d0
manumrighi <manuelmorichi08@gmail.com>
2022-11-15 16:22:21 -0300

    segundo commit

---
+++ ProyectoBE/Controllers/AuthenticationController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoBE.Data.Repository.Interfaces;
+using ProyectoBE.Models;
+using System.Text;
+using ProyectoBE.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace ProyectoBE.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly IUserRepository _userRepository;
+
+        public AuthenticationController(IConfiguration config, IUserRepository userRepository)
+        {
+            _config = config;
+            this._userRepository = userRepository;
+        }
+
+        [HttpPost("authenticate")]
+        public ActionResult<string> Autenticar(AuthenticationRequestBody authenticationRequestBody)
+        {
+            var user = _userRepository.ValidateUser(authenticationRequestBody);
+
+            if (user is null)
+                return Unauthorized();
+
+            var securityPassword = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_config["Authentication:SecretForKey"]));
+        }
+
+    }
+            
+}
--- ProyectoBE/Controllers/ContactController.cs
+++ ProyectoBE/Controllers/ContactController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ProyectoBE.Data.Repository.Implementations;
+using ProyectoBE.Data.Repository.Interfaces;
+using ProyectoBE.Entities;
+using ProyectoBE.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace ProyectoBE.Controllers
 {
@@ -8,7 +14,69 @@ namespace ProyectoBE.Controllers
     [Authorize]
     public class ContactController : ControllerBase
     {
+        private readonly IContactRepository _contactRepository;
+        private readonly IUserRepository _userRepository;
+        public ContactController(IContactRepository contactRepository, IUserRepository userRepository)
+        {
+            _contactRepository = contactRepository;
+            _userRepository = userRepository;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_contactRepository.GetAll());
+        }
 
-       
+        [HttpGet]
+        [Route("{Id}")]
+        public IActionResult GetOne(int Id)
+        {
+            return Ok(_contactRepository.GetAll().Where(x => x.Id == Id));
+        }
+
+        [HttpPost]
+        public IActionResult CreateContact(CreateAndUpdateContactDto createContactDto)
+        {
+            try
+            {
+                _contactRepository.Create(createContactDto);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Created("Created", createContactDto);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateContact(CreateAndUpdateContactDto dto)
+        {
+            try
+            {
+                _contactRepository.Update(dto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteContactById(int id)
+        {
+            try
+            {
+                if (User.Identity.Name == "Admin")
+                {
+                    _contactRepository.Delete(id);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+            return Ok();
+        }  
     }
 }
